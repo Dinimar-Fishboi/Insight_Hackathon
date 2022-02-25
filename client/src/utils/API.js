4a6b665d0265f8f3c761f10b1bc5f050b6e94021
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-02-25 14:40:24 +1100

    added api utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,8 @@
+import axios from 'axios';
+const apiURL = "http://localhost:3000/server"
+
 // Getting the API information to the front end
+const apiSearch = async (query) =>
+    axios.get(`${apiURL}`)
+
+    export default { apiSearch}
