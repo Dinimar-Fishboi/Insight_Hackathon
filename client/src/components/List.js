e1f338ed61399ff24c1c8a77ca4dfed0a779f14f
Dinimar Fish <tayla@tallisman.com.au>
2022-02-25 13:14:24 +1000

    setting up api calls to render info

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import getList from '../utils/API'
 
 const List = ({
     props
 }) => {
+
+    const [results, setResults] = useState([]);
+
+    const getServerDetails = (query) => {
+        const response = await getList(query)
+        setResults(response)
+    }
+
+    if (!results.length) (
+        <>
+        <p>
+            No data => check API
+        </p>
+        </>
+    )
 
     return (
         <>
             <h4>
                 This is the List component
             </h4>
+            <button
+            onClick={getServerDetails}
+            > Retrieve server details</button>
         </>
     )
 }
